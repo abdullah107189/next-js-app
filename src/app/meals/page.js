552ee6a21123a4a1f79fcd33650dd732bafee2bd
abdullah107189 <abdullah107189@gmail.com>
2025-02-13 20:24:01 +0600

    added meal content

--- src/app/meals/page.js
+++ src/app/meals/page.js
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import React, { useEffect, useState } from "react";
 const page = () => {
   const [meals, setMeals] = useState([]);
@@ -18,8 +19,28 @@ const page = () => {
   useEffect(() => {
     fetchMeal();
   }, [serach]);
-
-  return <div>meals length is : {meals.length}</div>;
+  console.log(meals);
+  return (
+    <div className="grid grid-cols-5 gap-4">
+      <div className="col-span-5 flex items-center justify-center my-5">
+        <input
+          onChange={(e) => setSearch(e.target.value)}
+          className="border px-5 rounded-md bg-gray-100"
+          type="text"
+          placeholder="Enter Title Name"
+        />
+      </div>
+      {meals?.map((meal) => (
+        <Link
+          href={`/meals/${meal?.idMeal}`}
+          key={meal?.idMeal}
+          className={`border p-2 rounded-lg bg-indigo-100 hover:bg-indigo-200`}
+        >
+          <p>{meal?.strMeal}</p>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default page;
